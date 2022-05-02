d103bf5e74e8e1e04421311daf518b1cc7caeb85
Jeferson Leite Borges <jeferson.borges@ihsmarkit.com>
2022-05-02 10:32:07 +0200

    Basic blog stuff, not properly working

--- ruby/blog-test/app/controllers/articles_controller.rb
+++ ruby/blog-test/app/controllers/articles_controller.rb
@@ -43,7 +43,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
