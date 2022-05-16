29a170997aeb333c95f384ec134937c2dbf493a4
Jeferson 'Shin <shin.jef@gmail.com>
2022-05-16 17:51:44 +0200

    Added room area sensor on the project

---
+++ godot/2022/QuickVania_godot3/Entities/RoomArea/RoomArea.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal player_entered_room(pos_x, pox_y)
+
+export(int) var map_global_position_x
+export(int) var map_global_position_y
+
+
+func _ready() -> void:
+	var con = connect("body_entered", self, "on_player_entered")
+
+	if con != OK:
+		print_debug("INFO:: Failed to connect")
+
+
+func on_player_entered(body: Node) -> void:
+	if body is Player:
+		emit_signal("player_entered_room", map_global_position_x, map_global_position_y)
