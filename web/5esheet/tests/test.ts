8fd5272faf0a19381b6d4f5e6efd6ae0f0968b1f
Jeferson 'Shin <shin.jef@gmail.com>
2022-06-05 17:20:16 +0200

    Create base project in svelte kit

--- web/5esheet/tests/test.ts
+++ web/5esheet/tests/test.ts
@@ -1,6 +1,14 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test("basic areas", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("text=stats");
+  await page.locator("text=info");
+  await page.locator("text=prof");
+  await page.locator("text=save");
+  await page.locator("text=skills");
+  await page.locator("text=profs");
+  await page.locator("text=combat");
+  await page.locator("text=weapons");
+  await page.locator("text=traits");
 });
