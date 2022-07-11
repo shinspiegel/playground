159468568630774e5d8493df3ef4d4a45a2d860a
Jeferson Leite Borges <jeferson.borges@ihsmarkit.com>
2022-07-11 13:07:07 +0200

    Update logic on 5esheet

--- web/5esheet/components/Info/index.tsx
+++ web/5esheet/components/Info/index.tsx
@@ -1,6 +1,4 @@
-import { UserInfo } from "os";
 import React from "react";
-import { UseInfo } from "../../hooks";
 import { InfoSlice } from "../../stores";
 import cn from "./index.module.scss";
 
@@ -11,32 +9,32 @@ export interface InfoProps {
 
 export const Info: React.FC<InfoProps> = ({ info = {} as InfoSlice, onChange = () => {} }) => (
   <div className={cn.container}>
-    <label>
+    <label className={cn.name}>
       <span>Character Name</span>
       <input name="name" value={info.name} onChange={onChange} />
     </label>
 
-    <label>
+    <label className={cn.class}>
       <span>Class</span>
       <input name="dndClass" value={info.dndClass} onChange={onChange} />
     </label>
 
-    <label>
+    <label className={cn.race}>
       <span>Race</span>
       <input name="race" value={info.race} onChange={onChange} />
     </label>
 
-    <label>
+    <label className={cn.player}>
       <span>Player</span>
       <input name="player" value={info.player} onChange={onChange} />
     </label>
 
-    <label>
+    <label className={cn.level}>
       <span>Level</span>
       <input name="level" type="number" value={info.level} onChange={onChange} />
     </label>
 
-    <label>
+    <label className={cn.experience}>
       <span>Experience</span>
       <input name="experience" type="number" value={info.experience} onChange={onChange} />
     </label>
--- web/5esheet/stores/slices/statsSlice.ts
+++ web/5esheet/stores/slices/statsSlice.ts
@@ -25,19 +25,21 @@ export const statsSlice = createSlice({
   reducers: {
     changeValueFor: (state, action: PayloadAction<{ name: string; value: number }>) => {
       const stat = state.list.find((f) => f.name === action.payload.name);
+      const value = parseInt(String(action.payload.value));
 
       if (stat) {
-        stat.value = action.payload.value;
-        stat.mod = statToMod(action.payload.value);
+        stat.value = value;
+        stat.mod = statToMod(value);
       }
     },
 
     changeModFor: (state, action: PayloadAction<{ name: string; value: number }>) => {
       const stat = state.list.find((f) => f.name === action.payload.name);
+      const value = parseInt(String(action.payload.value));
 
       if (stat) {
-        stat.value = modToStat(action.payload.value);
-        stat.mod = action.payload.value;
+        stat.value = modToStat(value);
+        stat.mod = value;
       }
     },
 
