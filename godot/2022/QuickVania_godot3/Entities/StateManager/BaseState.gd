a0aa037a3a6d00a94ab10435ae3e252044d99ec8
Jeferson 'Shin <shin.jef@gmail.com>
2022-07-17 19:35:46 +0200

    Removed debug from base state

--- godot/2022/QuickVania_godot3/Entities/StateManager/BaseState.gd
+++ godot/2022/QuickVania_godot3/Entities/StateManager/BaseState.gd
@@ -1,30 +1,23 @@
 class_name BaseState extends Node2D
 
-export var debug: bool = false
-
 var target = null
 
 
 func _ready() -> void:
-	if debug:
-		print_debug("INFO:: State ready [%s]" % [name])
+	pass
 
 
 func enter() -> void:
-	if debug:
-		print_debug("INFO:: State entered [%s]" % [name])
+	pass
 
 
 func exit() -> void:
-	if debug:
-		print_debug("INFO:: State exited [%s]" % [name])
+	pass
 
 
-func process(delta: float) -> void:
-	if debug:
-		print_debug("INFO:: State process [%s] on delta [%s]" % [name, delta])
+func process(_delta: float) -> void:
+	pass
 
 
 func check_change_state() -> void:
-	if debug:
-		print_debug("INFO:: Check if should change state in [%s]" % [name])
+	pass
