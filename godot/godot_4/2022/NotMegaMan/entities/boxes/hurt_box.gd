1b66f7abf1478b7f4a2e7e2e1350c1df0f887aad
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-11-03 15:06:45 +0100

    Added boxes

---
+++ godot/godot_4/2022/NotMegaMan/entities/boxes/hurt_box.gd
@@ -0,0 +1,26 @@
+class_name HurtBox extends Area2D
+
+signal hit_received(hit_box)
+
+@onready var shape: CollisionShape2D = $CollisionShape2d
+
+
+func _ready() -> void:
+	area_entered.connect(on_area_enter)
+
+
+func enable() -> void:
+	set_shape_disabled(false)
+
+
+func disable() -> void:
+	set_shape_disabled(true)
+
+
+func set_shape_disabled(value: bool) -> void:
+	shape.disabled = value
+
+
+func on_area_enter(hit_box) -> void:
+	if hit_box is HitBox:
+		hit_received.emit(hit_box)
