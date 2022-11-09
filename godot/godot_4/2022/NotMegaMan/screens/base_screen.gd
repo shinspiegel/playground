f0b7a9761616f5f7efdeb57a5f1958cedf03a88e
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-11-09 21:02:21 +0100

    Added base screen

--- godot/godot_4/2022/NotMegaMan/screens/base_screen.gd
+++ godot/godot_4/2022/NotMegaMan/screens/base_screen.gd
@@ -1,1 +1,10 @@
 class_name BaseScreen extends Control
+
+@export var initial_focus: NodePath
+
+func _ready() -> void:
+	if not initial_focus.is_empty():
+		var focus = get_node(initial_focus)
+		
+		if focus is Control:
+			focus.grab_focus()
