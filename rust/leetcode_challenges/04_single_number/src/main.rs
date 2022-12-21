6b1a1419f6b22b76e98ad22422e395d772501f16
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-12-21 19:03:38 +0100

    Added single number

--- rust/leetcode_challenges/04_single_number/src/main.rs
+++ rust/leetcode_challenges/04_single_number/src/main.rs
@@ -1,4 +1,13 @@
 fn solution(numbers: Vec<i32>) -> i32 {
+    for (i, n) in numbers.iter().enumerate() {
+        let mut cloned = numbers.clone();
+        cloned.remove(i);
+
+        if !cloned.contains(n) {
+            return n.clone();
+        }
+    }
+
     return 0;
 }
 
