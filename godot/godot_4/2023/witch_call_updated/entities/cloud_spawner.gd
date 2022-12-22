97c3748a1c8fb69fdc157b6777d8e4ab0fde0c6d
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-12-22 18:16:26 +0100

    Updated clouds to have logic for the size

--- godot/godot_4/2023/witch_call_updated/entities/cloud_spawner.gd
+++ godot/godot_4/2023/witch_call_updated/entities/cloud_spawner.gd
@@ -20,14 +20,6 @@ func size_changed():
 
 func spawn_clouds():
 	for n in randi_range(min_amount, max_amount):
-		var layer = randi_range(0,3)
 		var instance: Node2D = cloud_scene.instantiate()
-		
 		add_child(instance)
 		instance.global_position.x = randi_range(0, size.x)
-		
-		if(not layer == 0):
-			var amount = 1/float(layer)
-			instance.scale.x -= amount
-			instance.scale.y -= amount
-			instance.modulate = Color(1,1,1,amount)
--- godot/godot_4/2023/witch_call_updated/entities/clouds.gd
+++ godot/godot_4/2023/witch_call_updated/entities/clouds.gd
@@ -8,10 +8,18 @@ extends Sprite2D
 @onready var visibility: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 func _ready() -> void:
+	var layer = randi_range(0,3)
 	var selected_frame = randi_range(0, 4)
+	
 	speed += randi_range(-10, 10)
 	frame = selected_frame
 	visibility.screen_exited.connect(func(): queue_free())
+	
+	if(not layer == 0):
+		var amount = 1/float(layer)
+		scale.x -= amount
+		scale.y -= amount
+		modulate = Color(1,1,1,amount)
 
 
 func _process(delta: float) -> void:
