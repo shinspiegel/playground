25f329ace50970afd72c17477f0c5fe10820e3ef
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-12-23 07:52:42 +0100

    Added player movement

--- godot/godot_4/2023/witch_call_updated/entities/player.gd
+++ godot/godot_4/2023/witch_call_updated/entities/player.gd
@@ -1,16 +1,36 @@
 class_name Witch extends CharacterBody2D
 
-var direction = Vector2.ZERO
+@export var run_data: RunData
+@export var speed = 200.0
+@export var direction: float = 0.0
+@export var shoot: bool = false
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	check_direction_keys()
 	check_shoot_keys()
+	
+	apply_direction()
+	apply_shoot()
+	
+	move_and_slide()
+
+
+func apply_direction() -> void:
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed/10)
+
+
+func apply_shoot() -> void:
+	if shoot:
+		print("shoot!")
 
 
 func check_direction_keys() -> void:
-	pass
+	direction = Input.get_axis("left", "right")
 
 
 func check_shoot_keys() -> void:
-	pass
+	shoot = Input.is_action_pressed("shot")
