d78ca2493f0dc66813f55ab35bb9c408cb3c3aa2
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2023-02-04 10:13:31 +0100

    Added monster down and shoot

--- godot/godot_4/2023/witch_call_updated/waves/monsters/down_shoot.gd
+++ godot/godot_4/2023/witch_call_updated/waves/monsters/down_shoot.gd
@@ -3,5 +3,6 @@ extends MonsterBase
 @onready var shoot_spawn: MonsterShootSpawner = $MonsterShootSpawner
 
 
-func apply_special(_delta: float) -> void:
+func apply_special(delta: float) -> void:
+	apply_movement(delta)
 	shoot_spawn.shoot()
