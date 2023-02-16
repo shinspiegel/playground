4b969c2d1b9981c3cf1ecaaec8b6e3f7cf98a265
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2023-02-16 16:01:01 +0100

    Created first build and tested the build

--- godot/godot_4/2023/witch_call_updated/waves/wave_list/create_waves.sh
+++ godot/godot_4/2023/witch_call_updated/waves/wave_list/create_waves.sh
@@ -1,11 +1,10 @@
-MIN=$1
-MAX=$2
-FOLDER="$MIN-$MAX-$3"
-
-if [ ! -d "$FOLDER" ]; then
-  mkdir -p "$FOLDER"
-fi
+FOLDER=$1
+DIFF=$2
+MIN=$3
+MAX=$4
 
 for i in $(seq $MIN $MAX); do
-  cat wave_X.tres > "$FOLDER/wave_$i.tres"
+  FILENAME="wave-$DIFF-$i"
+  cat wave_X.tres > "$FOLDER/$FILENAME"
 done
+
