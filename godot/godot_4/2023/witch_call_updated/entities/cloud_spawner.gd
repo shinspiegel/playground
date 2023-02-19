6cfa496147dc5a0cfe489709883b87ca974fc944
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2023-02-19 12:15:15 +0100

    Added clouds as particles added hard levels

--- godot/godot_4/2023/witch_call_updated/entities/cloud_spawner.gd
+++ godot/godot_4/2023/witch_call_updated/entities/cloud_spawner.gd
@@ -1,23 +1,6 @@
 extends Node2D
 
-@export var cloud_scene: PackedScene
-@export var min_amount: int = 1
-@export var max_amount: int = 5
-
-@onready var delay: Timer = $Delay
+@onready var particles: CPUParticles2D = $CPUParticles2D
 
 func _ready() -> void:
-	delay.timeout.connect(on_time_out)
-
-
-func on_time_out() -> void:
-	for n in randi_range(min_amount, max_amount):
-		spawn_cloud()
-
-
-func spawn_cloud():
-	var instance: Node2D = cloud_scene.instantiate()
-	var pos_x = randi_range(0, ProjectSettings.get_setting("display/window/size/viewport_width"))
-	add_child(instance)
-	instance.global_position.x = pos_x
-
+	particles.set_emitting(true)
