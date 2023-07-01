aab505121aec6475ae276e650caf4c48c45c3287
Jeferson 'Shin <shin.jef@gmail.com>
2023-07-01 10:13:30 +0200

    Updated logic

---
+++ swift/locatorTest/LocatorTest/LocatorTest/LocatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocatorTestApp.swift
+//  LocatorTest
+//
+//  Created by Jeferson Leite Borges on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LocatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/locatorTest/LocatorTest/LocatorTestUITests/LocatorTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LocatorTestUITestsLaunchTests.swift
+//  LocatorTestUITests
+//
+//  Created by Jeferson Leite Borges on 01/07/23.
+//
+
+import XCTest
+
+final class LocatorTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
