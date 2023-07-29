3d7b187f7ac25b1297e4b774a3b80dc015e700ea
Jeferson 'Shin <shin.jef@gmail.com>
2023-07-29 10:15:36 +0200

    Fix player

--- godot/godot_4/2023/10_shopkeeper/Entities/Player/Player.gd
+++ godot/godot_4/2023/10_shopkeeper/Entities/Player/Player.gd
@@ -3,17 +3,16 @@ class_name Player extends CharacterBody2D
 const Speed: float = 500.0
 
 var direction: Vector2 = Vector2.ZERO
-var velocity: Vector2 = Vector2.ZERO
 
 
-func _physics_process(float delta):
+func _physics_process(delta: float) -> void:
 	apply_direction()
 	apply_movement()
 
 
-func apply_direction()
-	velocity = (Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalize()) * Speed
+func apply_direction() -> void:
+	velocity = (Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()) * Speed
 
 
-func apply_movement()
+func apply_movement() -> void:
 	move_and_slide()
