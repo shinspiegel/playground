74547b55b011c07a9bd44d6a3027c2a0852cec77
Jeferson 'Shin <shin.jef@gmail.com>
2023-08-13 13:56:24 +0200

    Added base for the crafting selection

--- godot/godot_4/2023/11_shopkeeper_again/Levels/AlchemyLab/AlchemyLab.gd
+++ godot/godot_4/2023/11_shopkeeper_again/Levels/AlchemyLab/AlchemyLab.gd
@@ -1,10 +1,12 @@
 extends BaseLevel
 
+@onready var craft_definition: Control = $Overlay/CraftDefinition
 
 func _ready() -> void:
 	super._ready()
 	CraftManager.started.connect(on_craft_start)
 	CraftManager.ended.connect(on_craft_ended)
+	craft_definition.hide()
 
 
 func on_craft_start() -> void:
--- godot/godot_4/2023/11_shopkeeper_again/Levels/BaseLevel.gd
+++ godot/godot_4/2023/11_shopkeeper_again/Levels/BaseLevel.gd
@@ -2,19 +2,23 @@ class_name BaseLevel extends Node2D
 
 @onready var player_inventory: Control = $Overlay/PlayerInventory
 @onready var sorted_object: Node2D = $SortedObject
+@onready var background: Control = $Overlay/Background
 
 
 func _ready() -> void:
 	player_inventory.hide()
+	background.hide()
 	GameManager.inventory_opened.connect(on_inventory_opened)
 	GameManager.inventory_closed.connect(on_inventory_closed)
 
 
 func on_inventory_opened() -> void:
 	sorted_object.set_deferred("process_mode", PROCESS_MODE_DISABLED)
+	background.show()
 	player_inventory.show()
 
 
 func on_inventory_closed() -> void:
 	player_inventory.hide()
+	background.hide()
 	sorted_object.set_deferred("process_mode", PROCESS_MODE_INHERIT)
