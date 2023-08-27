e42715ea51ce346fd60c3a3d59200d94c654b5ff
Jeferson 'Shin <shin.jef@gmail.com>
2023-08-27 13:00:25 +0200

    Started work on potion

--- godot/godot_4/2023/11_shopkeeper_again/Resources/InventoryItems/InventoryPotion.gd
+++ godot/godot_4/2023/11_shopkeeper_again/Resources/InventoryItems/InventoryPotion.gd
@@ -1,3 +1,5 @@
 class_name InventoryPotion extends InventoryItem
 
 @export_group("Potion")
+@export_range(0.1, 1.0, 0.1) var tick_time: float = 0.1
+@export_range(0.0, 1.0, 0.1) var duration: float = 0.0
