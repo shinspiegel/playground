d11eabf598de5a6b90149ac7c1089837e8b681f0
Jeferson 'Shin <shin.jef@gmail.com>
2023-09-03 14:48:21 +0200

    Created base level for testing

--- godot/godot_4/2023/11_shopkeeper_again/Entities/InteractableObjects/BreakableLoot/BreakableLoot.gd
+++ godot/godot_4/2023/11_shopkeeper_again/Entities/InteractableObjects/BreakableLoot/BreakableLoot.gd
@@ -2,16 +2,17 @@ extends InteractableObject
 
 @export_group("Loot Options")
 @export var possible_loot: Array[PossibleLoot]
+@export_range(1.0, 10.0, 0.1) var extra_push: float = 3.0
 
 
 func _on_interact() -> void:
-	for loot in possible_loot:
-		if randf_range(0.0, 1.0) < loot.chance:
-			var clone = loot.item.duplicate()
+	for loot_entry in possible_loot:
+		if randf_range(0.0, 1.0) < loot_entry.chance:
+			var clone = loot_entry.loot.duplicate()
 			
 			if clone is InventoryItem: 
 				clone.rand_direction()
-				clone.rand_spawn_force()
+				clone.rand_spawn_force(extra_push)
 			
 			GameManager.spawn_item(clone, global_position)
 	
--- godot/godot_4/2023/11_shopkeeper_again/Resources/InventoryItems/PossibleLoot.gd
+++ godot/godot_4/2023/11_shopkeeper_again/Resources/InventoryItems/PossibleLoot.gd
@@ -1,4 +1,4 @@
 class_name PossibleLoot extends Resource 
 
-@export_range(0.0, 1.0, 0.01) var chance: float 
+@export_range(0.0, 1.0, 0.05) var chance: float 
 @export var loot: Resource
