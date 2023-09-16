dad9ee9cbc6553ea67532cd3aea7da6d235ab42a
Jeferson 'Shin <shin.jef@gmail.com>
2023-09-16 14:51:30 +0200

    Update fixed player bullet to hit enemies

--- godot/godot_4/2023/12_infinity_side_runner/entities/bullet/bullet.gd
+++ godot/godot_4/2023/12_infinity_side_runner/entities/bullet/bullet.gd
@@ -2,6 +2,7 @@ class_name Bullet extends Area2D
 
 @export var direction: int = 1
 @export_range(0.0, 10.0, 0.1) var speed: float = 1.0
+@export var hit_effect: PackedScene
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 var __multiplier: float = 1000.0
@@ -17,6 +18,8 @@ func _physics_process(delta: float) -> void:
 
 
 func on_body_entered(_body: Node2D) -> void:
+	if hit_effect:
+		GameManager.create_node(hit_effect, global_position)
 	queue_free()
 
 
