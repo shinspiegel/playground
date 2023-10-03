f437ebb3d5c00c6d7104c2404b7018cbb0f0f598
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2023-10-03 12:45:06 +0200

    Added background music

--- godot/godot_4/2023/12_infinity_side_runner/entities/background_music/background_music.gd
+++ godot/godot_4/2023/12_infinity_side_runner/entities/background_music/background_music.gd
@@ -1,11 +1,21 @@
 class_name BackgroundMusic extends Node
 
-@export_enum("menu", "game") var music: String = "menu"
+@export_enum(
+	"elara_prelude",
+	"ancient_dynasties",
+	"neon_nexus",
+	"whispers_of_the_woodlands",
+	"game_over"
+) var music: String = "elara_prelude"
 
 const MUSIC = {
-	"menu": preload("res://entities/background_music/musics_resources/main_music.tres"),
-	"game": preload("res://entities/background_music/musics_resources/game_music.tres"),
+	"ancient_dynasties": preload("res://entities/background_music/musics_resources/ancient_dynasties.tres"), 
+	"elara_prelude": preload("res://entities/background_music/musics_resources/elara_prelude.tres"), 
+	"neon_nexus": preload("res://entities/background_music/musics_resources/neon_nexus.tres"), 
+	"whispers_of_the_woodlands": preload("res://entities/background_music/musics_resources/whispers_of_the_woodlands.tres"),
+	"game_over": preload("res://entities/background_music/musics_resources/game_over.tres"),
 }
 
 func _ready() -> void:
-	AudioManager.play_music(MUSIC[music])
+	if music and MUSIC.has(music):
+		AudioManager.play_music(MUSIC[music])
