1a71d48b4be2ddd4d279b5cd050e9d7a0fdfa1da
Jeferson 'Shin <shin.jef@gmail.com>
2023-12-02 10:05:00 +0100

    Updated swift project

---
+++ swift/locatorTest/UltraLocator/UltraLocator/UltraLocator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltraLocator
+//
+//  Created by Jeferson Leite Borges on 02/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/locatorTest/UltraLocator/UltraLocator/UltraLocator/UltraLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraLocatorApp.swift
+//  UltraLocator
+//
+//  Created by Jeferson Leite Borges on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UltraLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
