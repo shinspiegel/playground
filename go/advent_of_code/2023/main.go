2eb85ffdb7abd8f82c530fa933d069ffd52ecb12
Jeferson 'Shin <shin.jef@gmail.com>
2023-12-05 17:31:03 +0100

    Added day 3 partial completed

--- go/advent_of_code/2023/main.go
+++ go/advent_of_code/2023/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"advent-of-code-2023/day1"
 	"advent-of-code-2023/day2"
+	"advent-of-code-2023/day3"
 	"advent-of-code-2023/reader"
 	"fmt"
 )
@@ -14,8 +15,12 @@ func main() {
 	fmt.Printf("Day 1, Part 2::[%v]\n", d1p2)
 
 	d2p1 := day2.PartOne(reader.GetDay2(), &day2.Cube{Red: 12, Green: 13, Blue: 14})
-	fmt.Printf("Day 1, Part 1::[%v]\n", d2p1)
+	fmt.Printf("Day 2, Part 1::[%v]\n", d2p1)
 	d2p2 := day2.PartTwo(reader.GetDay2())
-	fmt.Printf("Day 1, Part 2::[%v]", d2p2)
+	fmt.Printf("Day 2, Part 2::[%v]\n", d2p2)
 
+	d3p1 := day3.PartOne(reader.GetDay2())
+	fmt.Printf("Day 3, Part 1::[%v]\n", d3p1)
+	d3p2 := day3.PartTwo(reader.GetDay2())
+	fmt.Printf("Day 3, Part 2::[%v]\n", d3p2)
 }
