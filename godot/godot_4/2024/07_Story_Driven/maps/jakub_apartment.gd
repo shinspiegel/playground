4e8fb35c58a924af791086ef49a757d416297ef5
Jeferson 'Shin <shin.jef@gmail.com>
2024-03-20 21:14:03 +0100

    Added extra messages on jakub apartment

--- godot/godot_4/2024/07_Story_Driven/maps/jakub_apartment.gd
+++ godot/godot_4/2024/07_Story_Driven/maps/jakub_apartment.gd
@@ -1,19 +1,30 @@
 extends LevelMap
 
+@export_group("Chapter 1")
+@export var bubble_timer: Timer
+@export_subgroup("Boxes")
 @export var boxes_interactor: Interactable
 @export var boxes_message: MessageData
+@export_subgroup("Sofa")
+@export var sofa_interactable: Interactable
+@export var sofa_message: MessageData
 
 
 func _ready() -> void:
-	boxes_interactor.focus.connect(on_box_interact)
+	var list = [
+		[boxes_interactor, boxes_message],
+		[sofa_interactable, sofa_message],
+	]
+
+	for entry in list:
+		entry[0].focus.connect(on_focus.bind(PartyManager.get_leader(), entry[1]))
 
 	prepare()
 
 
 
-func on_box_interact() -> void:
-	MessageManager.create_bubble(
-		get_tree().root,
-		boxes_message,
-		PartyManager.get_leader().message_pos.global_position,
-	)
+func on_focus(actor: Actor, message: MessageData) -> void:
+	if bubble_timer.is_stopped():
+		bubble_timer.start()
+		MessageManager.create_bubble(actor, message, actor.message_pos.global_position)
+
