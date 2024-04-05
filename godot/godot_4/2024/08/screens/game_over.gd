366af9a411918f2eda126090ed5056de86d5ee3b
Jeferson 'Shin <shin.jef@gmail.com>
2024-04-05 09:48:33 +0200

    Starting platform

---
+++ godot/godot_4/2024/08/screens/game_over.gd
@@ -0,0 +1,25 @@
+class_name GameOverScreen extends Control
+
+@onready var start: Button = %Start
+@onready var credits: Button = %Credits
+@onready var quit: Button = %Quit
+
+func _ready() -> void:
+	AudioManager.play_music(AudioManager.MUSIC_LIST[1])
+	start.pressed.connect(on_start)
+	credits.pressed.connect(on_credits)
+	quit.pressed.connect(on_quit)
+	await get_tree().create_timer(1).timeout
+	start.grab_focus()
+
+
+func on_start() -> void:
+	SceneManager.change_to_file(SceneManager.SCENES.main)
+
+
+func on_credits() -> void:
+	SceneManager.change_to_file(SceneManager.SCENES.credits)
+
+
+func on_quit() -> void:
+	SceneManager.quit()
