e4a0dfa9167cef46321f852ef968cb788b26f2a3
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2024-04-10 11:10:52 +0200

    Reduced delay from 0.5 to 0.2

--- godot/godot_4/2024/08/singletons/scene_manager.gd
+++ godot/godot_4/2024/08/singletons/scene_manager.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+signal faded_in()
+signal faded_out()
+
+const COLOR_FILL = Color(1,1,1,1)
+const COLOR_OUT = Color(1,1,1,0)
+const FADE_DELAY = 0.2
+
 const SCENES = {
 	"main": "res://main.tscn",
 	"start": "res://screens/start_screen.tscn",
@@ -8,9 +15,6 @@ const SCENES = {
 	"game_over": "res://screens/game_over.tscn",
 }
 
-signal loading_entered()
-signal loading_exited()
-
 @onready var control: Control = $Control
 @onready var progress_bar: ProgressBar = $Control/ProgressBar
 
@@ -18,11 +22,11 @@ var __is_loading_visible: bool = false
 var __next_scene_path: String = ""
 var __loading_scene_status = ResourceLoader.THREAD_LOAD_INVALID_RESOURCE
 var __progress: Array[float] = []
-var __scene_delay: float = 0.5
+var __scene_delay: float = 0.3
 
 
 func _ready() -> void:
-	control.modulate = Color(1,1,1,0)
+	control.modulate = COLOR_OUT
 
 
 func _process(_delta: float) -> void:
@@ -30,17 +34,17 @@ func _process(_delta: float) -> void:
 		__check_loading_state()
 
 
-func reload(delay: float = 0.5) -> void:
+func reload(delay: float = 0.1) -> void:
 	show_loading()
-	await loading_entered
+	await faded_in
 	get_tree().reload_current_scene()
 	await get_tree().create_timer(delay).timeout
 	hide_loading()
 
 
-func change_to_file(filepath: String, delay: float = 0.5) -> void:
+func change_to_file(filepath: String, delay: float = 0.3) -> void:
 	show_loading()
-	await loading_entered
+	await faded_in
 	__next_scene_path = filepath
 	__scene_delay = delay
 	ResourceLoader.load_threaded_request(filepath)
@@ -51,19 +55,19 @@ func show_loading() -> void:
 	if not __is_loading_visible:
 		__clean_up()
 		var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(control, "modulate", Color(1,1,1,1), 0.2)
+		tween.tween_property(control, "modulate", COLOR_FILL, FADE_DELAY)
 		await tween.finished
-		loading_entered.emit()
+		faded_in.emit()
 		__is_loading_visible = true
 
 
 func hide_loading() -> void:
 	if __is_loading_visible:
 		var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT)
-		tween.tween_property(control, "modulate", Color(1,1,1,0), 0.2)
+		tween.tween_property(control, "modulate", COLOR_OUT, FADE_DELAY)
 		await tween.finished
 		__clean_up()
-		loading_exited.emit()
+		faded_out.emit()
 
 
 func quit() -> void:
