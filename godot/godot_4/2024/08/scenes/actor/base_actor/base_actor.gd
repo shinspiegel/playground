b087b62b259541a6e2ed903032e4e5d35c6ed36f
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2024-04-10 09:46:36 +0200

    Move inputs to be part of the BaseActor

--- godot/godot_4/2024/08/scenes/actor/base_actor/base_actor.gd
+++ godot/godot_4/2024/08/scenes/actor/base_actor/base_actor.gd
@@ -1,6 +1,7 @@
 class_name BaseActor extends CharacterBody2D
 
 @export var data: ActorData
+@export var input: BaseInputs
 @export var state_machine: StateMachine
 @onready var flip_enabled_node: Node2D = %FlipEnabled
 
--- godot/godot_4/2024/08/scenes/actor/player/player.gd
+++ godot/godot_4/2024/08/scenes/actor/player/player.gd
@@ -1,7 +1,4 @@
 class_name Player extends BaseActor
 
-@export var input: BaseInputs
-
-
 func _physics_process(delta: float) -> void:
 	state_machine.update(delta)
