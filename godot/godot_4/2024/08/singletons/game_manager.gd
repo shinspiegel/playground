0fa281ff3067ae042ce8ebc4b28ab5b6342bd1a5
Jeferson 'Shin <shin.jef@gmail.com>
2024-04-20 10:10:43 +0200

    Add funcionality to instanciate damage_number

--- godot/godot_4/2024/08/singletons/game_manager.gd
+++ godot/godot_4/2024/08/singletons/game_manager.gd
@@ -1,1 +1,18 @@
 extends Node
+
+const damage_number: Resource = preload("res://scenes/damageables/damage_number/damage_number.tscn")
+
+
+func spawn_damage_number(damage: Damage, pos: Vector2 = Vector2.ZERO, pos_variation: Vector2 = Vector2(20,20),parent: Node = null) -> void:
+	var instance: DamageNumber = damage_number.instantiate()
+	instance.damage = damage
+
+	if parent == null:
+		parent = get_tree().root
+	
+	parent.add_child(instance)
+
+	pos.x += randf_range(-pos_variation.x, pos_variation.x)
+	pos.y += randf_range(-pos_variation.y, pos_variation.y)
+
+	instance.global_position = pos
