f5044e296d071bd2d92385ca6f3d83b97c7a8f41
Jeferson 'Shin <shin.jef@gmail.com>
2024-04-27 09:59:21 +0200

    Add enemy idle and wander state

--- godot/godot_4/2024/08/scenes/actor/enemy/enemy_states/enemy_idle.gd
+++ godot/godot_4/2024/08/scenes/actor/enemy/enemy_states/enemy_idle.gd
@@ -13,6 +13,13 @@ func enter() -> void:
 	idle_duration.start()
 
 
+func update(delta: float) -> void:
+	enemy.apply_gravity(delta)
+	enemy.apply_direction(0, enemy.data.friction_land, 0.9)
+	enemy.move_and_slide()
+	enemy.check_flip(enemy.direction)
+
+
 func on_idle_end() -> void:
 	state_machine.change_state(next_state.name)
 
--- godot/godot_4/2024/08/scenes/actor/enemy/enemy_states/enemy_wander.gd
+++ godot/godot_4/2024/08/scenes/actor/enemy/enemy_states/enemy_wander.gd
@@ -1,1 +1,28 @@
 extends EnemyState
+
+@export var floor_raycast: RayCast2D
+@export var wall_raycast: RayCast2D
+@export var next_state: EnemyState
+@export var delay_timer: Timer
+
+
+func enter() -> void:
+	play_anim()
+	delay_timer.start()
+
+	if not floor_raycast.is_colliding() or wall_raycast.is_colliding():
+		enemy.direction *= -1
+
+
+func update(delta: float) -> void:
+	enemy.apply_gravity(delta)
+	enemy.apply_direction(enemy.direction, enemy.data.friction_land, 0.9)
+	enemy.move_and_slide()
+	enemy.check_flip(enemy.direction)
+
+	if wall_raycast.is_colliding():
+		enemy.direction *= -1
+
+	if delay_timer.time_left <= 0.0 and not floor_raycast.is_colliding() or wall_raycast.is_colliding():
+		state_machine.change_state(next_state.name)
+
