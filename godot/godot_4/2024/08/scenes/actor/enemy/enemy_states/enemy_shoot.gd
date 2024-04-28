61e96e36aa288c099c2cfe3db851525730f6196b
Jeferson 'Shin <shin.jef@gmail.com>
2024-04-28 12:10:33 +0200

    Added base EnemyShoot class

---
+++ godot/godot_4/2024/08/scenes/actor/enemy/enemy_states/enemy_shoot.gd
@@ -0,0 +1,30 @@
+extends EnemyState
+
+@export var anim_player: AnimationPlayer
+@export var next_state: EnemyState
+
+
+func _ready() -> void:
+	anim_player.animation_finished.connect(on_anim_finished)
+
+
+func enter() -> void:
+	play_anim()
+
+
+func update(delta: float) -> void:
+	enemy.apply_gravity(delta)
+	enemy.move_and_slide()
+
+
+func on_anim_finished(anim: String) -> void:
+	if anim == name:
+		if not next_state == null:
+			state_machine.change_state(next_state.name)
+		else:
+			state_machine.change_initial()
+
+
+
+
+
