e0628a05948d14d5d8982a78e9b44e957837791a
Jeferson 'Shin <shin.jef@gmail.com>
2024-05-05 13:05:01 +0200

    Added hitpoints to block

--- godot/godot_4/2024/08/scenes/actor/player/player_block/player_block.gd
+++ godot/godot_4/2024/08/scenes/actor/player/player_block/player_block.gd
@@ -1,9 +1,28 @@
 class_name PlayerBlock extends CharacterBody2D
 
-@export var actor: ActorData
+@onready var damage_receiver: DamageReceiver = %DamageReceiver
+@onready var damage_pos_spawn: Node2D = %DamagePos
+
+var max_hp: int = 30
+var current_hp: int = 30
+
+
+func _ready() -> void:
+	damage_receiver.receive_damage.connect(on_damage_receive)
+	current_hp = max_hp
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	move_and_slide()
+
+
+func on_damage_receive(damage: Damage) -> void:
+	current_hp = clampi(current_hp - damage.amount, 0, max_hp)
+	GameManager.spawn_damage_number(damage, damage_pos_spawn.global_position)
+
+	if current_hp <= 0:
+		queue_free()
+
