7bce68d6640337a1f09bdb497dd9ec9d655b53a7
Jeferson 'Shin <shin.jef@gmail.com>
2024-05-08 18:31:31 +0200

    Updated assets and removed inner dependency for shooter

--- godot/godot_4/2024/08/level/level_objects/one_way_block/one_way_block.gd
+++ godot/godot_4/2024/08/level/level_objects/one_way_block/one_way_block.gd
@@ -1,13 +1,29 @@
 class_name OneWayBlock extends Node2D
 
-@export var player_input: PlayerInput
-@export var shape: CollisionShape2D
+@onready var player_input: PlayerInput = $PlayerInput
+@onready var body: PhysicsBody2D = $Body
+
+var __shapes: Array[CollisionShape2D] = []
+var __state: bool = false
+
+
+func _ready() -> void:
+	for child in body.get_children():
+		if child is CollisionShape2D:
+			__shapes.append(child)
+			child.disabled = __state
 
 
 func _physics_process(_delta: float) -> void:
-	if player_input.down >= 0.2 and not shape.disabled:
-		shape.disabled = true
+	if player_input.down >= 0.2 and not __state:
+		set_all(true)
 
-	if player_input.down < 0.2 and shape.disabled:
-		shape.disabled = false
+	if player_input.down < 0.2 and __state:
+		set_all(false)
+
+
+func set_all(state: bool) -> void:
+	__state = state
+	for item in __shapes:
+		item.disabled = state
 
